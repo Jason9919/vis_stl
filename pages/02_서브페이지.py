310cba6c483f25a94a66603567f2abec73916a1e
Jason9919 <164982832+Jason9919@users.noreply.github.com>
2024-10-15 11:45:41 +0900

    Add files via upload

---
+++ pages/02_서브페이지.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# 텍스트 출력
+st.header('🚗🚗🚗🚗 서브 페이지 2 입니다.')
+st.write('')  # 빈 줄 삽입
+
+st.write('# 마크다운 H1 : st.write()')
+st.write(' 마크다운 H3 : st.write()')
+st.write('- 목록')
+st.write('')
